15486bd7adbb22c2b079f1d1a51c9ed0e2e80a05
titi <tanguy.dhautefeuille@gmail.com>
2019-06-27 20:43:27 +0200

    Nombre aléatoire V1

---
+++ Exercices/nombre-aléatoire.js
@@ -0,0 +1,33 @@
+//Exercice 1.1.1: Trouver un nombre aléatoire
+function nombreAleatoire()
+{
+  //Genérateur de nombre entier entre 0 et 100
+  var gNb = Math.floor(Math.random() * 101);
+  console.log("Le résultat aléatoir est: " + gNb);
+
+  var nbPropose = parseInt(prompt('Sélectioner un nombre entre 1 et 100'));
+  console.log("Le résultat fournit est: " + nbPropose);
+
+  var result = false;
+
+  while (result != true) {
+
+    if (gNb === nbPropose) {
+      alert("Bravo ! Les nombres sont exacts");
+      var result = true;
+
+    } else if (nbPropose > gNb) {
+      var nbPropose = parseInt(prompt("Le nombre est inférieur au tien"));
+      var result = false;
+
+    } else if (nbPropose < gNb) {
+      var nbPropose = parseInt(prompt("Le nombre est supérieur au tien"));
+      var result = false;
+
+    } else {
+      alert("Tu peux recommencer le jeu");
+      var result = true;
+    }
+    console.log("Résultat est " + result);
+  }
+}
