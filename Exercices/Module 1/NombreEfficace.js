ee5314431155a701da06836a0f63de8a7de78490
titi <tanguy.dhautefeuille@gmail.com>
2019-07-03 16:03:44 +0200

    update

---
+++ Exercices/Module 1/NombreEfficace.js
@@ -0,0 +1,5 @@
+//Exercice 1.1.2 : Trouver un nombre saisi par l’utilisateur efficacement
+function nombreEfficace()
+{
+  var nbPropose = parseInt(prompt('Sélectioner un nombre entre 1 et 100'));
+}
