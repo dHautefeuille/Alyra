503b6957b151e37d94d7db074087f296749b45d1
titi <tanguy.dhautefeuille@gmail.com>
2019-08-18 03:30:55 +0200

    VarInt vers Decimal

--- Exercices/defi/script.js
+++ Exercices/defi/script.js
@@ -18,20 +18,53 @@ document.getElementById("convertirh").addEventListener("click", event => {
           decimalh = convertHexaToDeci(hexah)
           document.getElementById("decimalh").innerHTML = deci
         })
-        function convertHexaLittleEndienToHexa(h) {
-            if (h.length % 2 == 1) {
-                h = '0' + h
-            }
-            let retour = "";
-            for (let i = 0; i < h.length - 1; i += 2) {
-                retour = h[i] + h[i + 1] + retour;
-            }
-            return retour;
-        }
-        document.getElementById("convertirPE").addEventListener("click", event => {
-            hle = document.getElementById("hle").value
-            hexa = convertHexaLittleEndienToHexa(hle)
-            document.getElementById("hexadecimalPE").innerHTML = "0x" + hexa
-            document.getElementById("decimalPE").innerHTML = parseInt(hexa, 16)
+function convertHexaLittleEndienToHexa(h) {
+  if (h.substring (0, 2) == "0x")
+    h = h.substring (2)
+    if (h.length % 2 == 1) {
+        h = '0' + h
+    }
+    let retour = "";
+    for (let i = 0; i < h.length - 1; i += 2) {
+        retour = h[i] + h[i + 1] + retour;
+    }
+    return retour;
+}
+document.getElementById("convertirPE").addEventListener("click", event => {
+    hle = document.getElementById("hle").value
+    hexa = convertHexaLittleEndienToHexa(hle)
+    document.getElementById("hexadecimalPE").innerHTML = "0x" + hexa
+})
 
-        })
+function convertVarIntToDeci(v){
+  if (v.substring (0, 2) == "0x"){
+    v= v.substring(2)
+  }
+  let VarInt = v.substring (0,2);
+  let resultat = ""
+  console.log(VarInt);
+  if (VarInt == "fd" ){
+    resultat = v.substring (2,6)
+    //console.log(v.substring (2,6));
+    resultat = convertHexaLittleEndienToHexa(resultat)
+    resultat = convertHexaToDeci(resultat)
+  } else if (VarInt == "fe"){
+    resultat = v.substring (2,10)
+    resultat = convertHexaLittleEndienToHexa(resultat)
+    resultat = convertHexaToDeci(resultat)
+  } else if (VarInt == "ff"){
+    resultat = v.substring (2,18)
+    resultat = convertHexaLittleEndienToHexa(resultat)
+    resultat = convertHexaToDeci(resultat)
+  } else {
+    resultat = convertHexaLittleEndienToHexa(v)
+    resultat = convertHexaToDeci(resultat)
+  }
+  //console.log(resultat);
+  return resultat
+}
+document.getElementById("convertirV").addEventListener("click", event => {
+    varintv = document.getElementById("VarIntV").value
+    varinRV = convertVarIntToDeci(varintv)
+    document.getElementById("decimalV").innerHTML = varinRV
+})
